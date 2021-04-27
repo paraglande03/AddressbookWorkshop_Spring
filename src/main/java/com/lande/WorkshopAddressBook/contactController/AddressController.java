1fcff4a297003f9e3bd6417d58335361475168f4
paraglande03 <paraglande03@gmail.com>
2021-04-27 10:49:46 +0530

    Refactored: controller to return Response Entity

--- src/main/java/com/lande/WorkshopAddressBook/contactController/AddressController.java
+++ src/main/java/com/lande/WorkshopAddressBook/contactController/AddressController.java
@@ -1,5 +1,7 @@
 package com.lande.WorkshopAddressBook.contactController;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,13 +10,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lande.WorkshopAddressBook.dto.ResponseDTO;
+
 @RestController
 @RequestMapping("/address")
 public class AddressController {
 	
 	@GetMapping("/getall")
-	public String getAllAddresses() {
-		return "Get call succesful for all addresses";
+	public ResponseEntity<ResponseDTO> getAllAddresses() {
+		ResponseDTO responseDTO = new ResponseDTO("success", getAllAddresses());
+		return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.OK);
 	}
 	
 	@GetMapping("/get/{Id}")
