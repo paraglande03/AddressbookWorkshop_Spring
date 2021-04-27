762124c8ce35e91df19476199fde31cc44c3b628
paraglande03 <paraglande03@gmail.com>
2021-04-27 11:30:13 +0530

    Added : Service method to create address

--- src/main/java/com/lande/WorkshopAddressBook/service/AddressService.java
+++ src/main/java/com/lande/WorkshopAddressBook/service/AddressService.java
@@ -5,15 +5,26 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+
+import com.lande.WorkshopAddressBook.dto.AddressDTO;
 import com.lande.WorkshopAddressBook.model.AddressData;
 
 @Service
-public class AddressService {
+public class AddressService implements AddressServiceInterface{
 	
 	List<AddressData> list = new ArrayList<>();
 	
 	public List<AddressData> getAllAddress(){
 		return list;
 	}
+
+	@Override
+	public AddressData createAddress(AddressDTO dto) {
+		AddressData addressData = new AddressData(list.size()+1, dto);
+		list.add(addressData);
+		return addressData;
+	}
+
+	
 
 }
---
+++ src/main/java/com/lande/WorkshopAddressBook/service/AddressServiceInterface.java
@@ -0,0 +1,16 @@
+package com.lande.WorkshopAddressBook.service;
+
+import java.util.List;
+
+import com.lande.WorkshopAddressBook.dto.AddressDTO;
+import com.lande.WorkshopAddressBook.model.AddressData;
+
+public interface AddressServiceInterface {
+	
+	public List<AddressData> getAllAddress();
+	
+//	public AddressData getAddressById(int id );
+	
+	public AddressData createAddress(AddressDTO dto);
+
+}
