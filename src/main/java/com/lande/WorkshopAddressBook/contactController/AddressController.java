84537572a2465f363b8daf2009861497573aff41
paraglande03 <paraglande03@gmail.com>
2021-04-27 11:35:10 +0530

    Refactored : controller function to acces service method

--- src/main/java/com/lande/WorkshopAddressBook/contactController/AddressController.java
+++ src/main/java/com/lande/WorkshopAddressBook/contactController/AddressController.java
@@ -1,7 +1,11 @@
 package com.lande.WorkshopAddressBook.contactController;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,13 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lande.WorkshopAddressBook.dto.ResponseDTO;
 import com.lande.WorkshopAddressBook.model.AddressData;
+import com.lande.WorkshopAddressBook.service.AddressServiceInterface;
 
 @RestController
 @RequestMapping("/address")
 public class AddressController {
 	
+	@Autowired
+	private AddressServiceInterface Service;
+	
 	@GetMapping("/getall")
-	public ResponseEntity<ResponseDTO> getAllAddresses(AddressData addressData) {
+	public ResponseEntity<ResponseDTO> getAllAddresses() {
+		List<AddressData> addressData = Service.getAllAddress();
 		ResponseDTO responseDTO = new ResponseDTO("success", addressData);
 		return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.OK);
 	}
