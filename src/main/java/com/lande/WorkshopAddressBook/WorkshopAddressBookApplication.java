d066281fa0f6d4965f7239b46f0ec6add2557144
paraglande03 <paraglande03@gmail.com>
2021-04-27 10:08:25 +0530

    Added: Project configuration

---
+++ src/main/java/com/lande/WorkshopAddressBook/WorkshopAddressBookApplication.java
@@ -0,0 +1,13 @@
+package com.lande.WorkshopAddressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopAddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopAddressBookApplication.class, args);
+	}
+
+}
