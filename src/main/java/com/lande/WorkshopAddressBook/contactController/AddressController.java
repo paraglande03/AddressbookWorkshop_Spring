391831f38abc44bac7bb5297ddfe3e64c86fbae9
paraglande03 <paraglande03@gmail.com>
2021-04-27 10:32:41 +0530

    Added: Controller function for getting address by Id

--- src/main/java/com/lande/WorkshopAddressBook/contactController/AddressController.java
+++ src/main/java/com/lande/WorkshopAddressBook/contactController/AddressController.java
@@ -2,6 +2,7 @@ package com.lande.WorkshopAddressBook.contactController;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,11 @@ public class AddressController {
 	@GetMapping("/getall")
 	public String getAllAddresses() {
 		return "Get call succesful for all addresses";
+	}
+	
+	@GetMapping("/get/{Id}")
+	public String getAddressById(@PathVariable("Id") int Id) {
+		return "Get call succesful for id :"+Id;
 	}
 	
 	@PostMapping("/create")
