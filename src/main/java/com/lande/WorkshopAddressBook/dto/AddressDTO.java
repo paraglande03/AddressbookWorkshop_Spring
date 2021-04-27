da96b8620054af08a9112998a4801ef387ce4d16
paraglande03 <paraglande03@gmail.com>
2021-04-27 10:41:09 +0530

    Added: Constructor and toString method for DTO

--- src/main/java/com/lande/WorkshopAddressBook/dto/AddressDTO.java
+++ src/main/java/com/lande/WorkshopAddressBook/dto/AddressDTO.java
@@ -5,5 +5,20 @@ public class AddressDTO {
 	public String city;
 	public String state;
 	public long zip;
+	
+	@Override
+	public String toString() {
+		return "AddressDTO [address=" + address + ", city=" + city + ", state=" + state + ", zip=" + zip + "]";
+	}
+
+	public AddressDTO(String address, String city, String state, long zip) {
+		super();
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	
+	
 
 }
