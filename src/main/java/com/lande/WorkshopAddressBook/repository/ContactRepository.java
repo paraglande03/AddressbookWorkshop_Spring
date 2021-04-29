62aab6994a0b754b59ed72cde1454b65f9fc4f29
paraglande03 <paraglande03@gmail.com>
2021-04-29 19:23:09 +0530

    Refactored: address repository to give uuid value

--- src/main/java/com/lande/WorkshopAddressBook/repository/ContactRepository.java
+++ src/main/java/com/lande/WorkshopAddressBook/repository/ContactRepository.java
@@ -1,13 +1,14 @@
 package com.lande.WorkshopAddressBook.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.lande.WorkshopAddressBook.model.Contact;
 
-public interface ContactRepository extends JpaRepository<Contact,Integer> {
+public interface ContactRepository extends JpaRepository<Contact,UUID> {
 	
 	@Query(value = " select * FROM contact_details ORDER BY f_name ASC",nativeQuery = true)
 		List<Contact> SortingContactAsc();
